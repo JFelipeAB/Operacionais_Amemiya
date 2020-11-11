d70d764bdb6e3af7940cfc82f493066cf605509c
unknown <joao-alves-borges@hotmail.com>
2020-11-11 09:53:50 -0300

    Criando projeto

---
+++ Documentação/ReadMe.txt
@@ -0,0 +1,6 @@
+Programa Console com diversos bat para executar funções operacionais do TI 
+
+Funções
+-Relatorio controle das impressões 
+
+
---
+++ Programa/OperacionaisTI/OperacionaisTI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OperacionaisTI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu_Iniciar();
+            Console.WriteLine("Encerrando");
+            Console.ReadLine();
+            
+        }
+        static void Menu_Iniciar()        
+            {
+            Console.WriteLine("Operacionais TI/n");
+            Console.WriteLine("Selecione a operação desejava\n1 - Relatorio Controle de impressões\n2 - Em breve");
+            int op = Convert.ToInt16(Console.ReadLine());
+
+        }
+    }
+}
